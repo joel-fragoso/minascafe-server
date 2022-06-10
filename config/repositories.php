39bffb3ae3d2cc3765de404529f613905dd22e19
felzika <fel_2@live.com>
2022-06-10 20:11:57 -0300

    Injeção de dependências dos repositórios
    Foi adicionado uma configuração para injeção de denpendências das categorias e produtos.

--- config/repositories.php
+++ config/repositories.php
@@ -4,11 +4,21 @@ declare(strict_types=1);
 
 use function DI\autowire;
 use DI\ContainerBuilder;
+use Doctrine\ORM\EntityManagerInterface;
+use Minascafe\Category\Infrastructure\Persistence\Doctrine\Entity\Category;
+use Minascafe\Product\Infrastructure\Persistence\Doctrine\Entity\Product;
 use Minascafe\Category\Domain\Repository\CategoryRepositoryInterface;
+use Minascafe\Product\Domain\Repository\ProductRepositoryInterface;
+use Psr\Container\ContainerInterface;
 use Minascafe\Category\Infrastructure\Persistence\Doctrine\Repository\CategoryRepository;
 
 return function (ContainerBuilder $containerBuilder) {
     $containerBuilder->addDefinitions([
-        CategoryRepositoryInterface::class => autowire(CategoryRepository::class),
+        CategoryRepositoryInterface::class => function (ContainerInterface $c) {
+			return $c->get(EntityManagerInterface::class)->getRepository(Category::class);
+		},
+		ProductRepositoryInterface::class => function (ContainerInterface $c) {
+			return $c->get(EntityManagerInterface::class)->getRepository(Product::class);
+		}
     ]);
 };

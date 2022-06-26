bdcdd11b3bf1f1d3a07cb1d388bc8a67ba3a3706
felzika <fel_2@live.com>
2022-06-25 23:31:16 -0300

    refactor: Cors multiplo dominios
    Adiciona Middleware para CORS `CorsMiddleware`.
    Refatora CORS para usar múltiplos domínios.
    
    Closes #39

--- config/middleware.php
+++ config/middleware.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use Minascafe\Shared\Infrastructure\Http\Middleware\CorsMiddleware;
 use Selective\Validation\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
 
 return function (App $app) {
     $app->add(ValidationExceptionMiddleware::class);
+    $app->add(CorsMiddleware::class);
 };

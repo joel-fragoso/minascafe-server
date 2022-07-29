edd92ea35bc61119687bd3794750bce0e8d45b56
Joel Fragoso <joelfragoso85@icloud.com>
2022-07-29 09:08:27 -0300

    bugfix: corrige o status code
    - Corrigido a verificação do status code quando for maior que 3 caracteres.
    
    Closes #73

--- src/Shared/Infrastructure/Http/Controller/BaseController.php
+++ src/Shared/Infrastructure/Http/Controller/BaseController.php
@@ -9,14 +9,14 @@ use Psr\Http\Message\ResponseInterface as Response;
 abstract class BaseController
 {
     /**
-     * @param array<int|string, int|string|object|array<object|string, int|string|array<int, string>>> $payload
+     * @param array<int|string, mixed> $payload
      */
     public function jsonResponse(Response $response, array $payload = [], int $code = 200): Response
     {
         $json = json_encode($payload, \JSON_PRETTY_PRINT);
         $response->getBody()->write($json);
 
-        if (0 === $code) {
+        if (0 === $code || 1000 <= $code) {
             $code = 500;
         }
 

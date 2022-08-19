6225d5d25b528898c1844b3d7c518bb8e0819d04
Joel Fragoso <joelfragoso85@icloud.com>
2022-08-19 12:58:52 -0300

    refactor: ajusta o tratamento de erro
    - Ajusta o tratamento de erro da API.
    
    Closes #77

---
+++ src/Shared/Application/Action/ActionPayload.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Minascafe\Shared\Application\Action;
+
+use JsonSerializable;
+use ReturnTypeWillChange;
+
+class ActionPayload implements JsonSerializable
+{
+    public function __construct(
+        private readonly int $statusCode = 200,
+        private readonly mixed $data = null,
+        private readonly ?ActionError $error = null
+    ) {
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    public function getError(): ?ActionError
+    {
+        return $this->error;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    #[ReturnTypeWillChange]
+    public function jsonSerialize(): array
+    {
+        $payload = [
+            'statusCode' => $this->statusCode,
+        ];
+
+        if ($this->data !== null) {
+            $payload['data'] = $this->data;
+        } elseif ($this->error !== null) {
+            $payload['error'] = $this->error;
+        }
+
+        return $payload;
+    }
+}
